7fcd263b70e5bc4e2d1903a4477630af89b27504
BW329rogerwang <BW329@sussex.ac.uk>
2025-03-31 16:14:20 +0100

    Update Land.cs

--- Assets/DFW/script 鑴氭湰/Land.cs
+++ Assets/DFW/script 鑴氭湰/Land.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using TMPro;
 
 // 地块类型
-public enum LandType { Normal, Commercial, Event, Start, Jail, Hospital, Chance, Tax, Station, Utilities, Community }
+public enum LandType { Normal, Free Parking, Event, Start, Jail, Opportunity Koncks,Pot Luck Tax, Station, Utilities, Community }
 
 public class Land : MonoBehaviour
 {
